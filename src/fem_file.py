288f4b415400c20f6bef319cdf0365a3b2096ff4
Eric <eric.meunier10@gmail.com>
2020-12-24 12:21:09 -0500

    Successfully parsed FEM file.

--- src/fem_file.py
+++ src/fem_file.py
@@ -16,13 +16,21 @@ class FEMFile:
         self.elevation = 0.
         self.units = '%Ht'
         self.current = 1.
-        self.tx_moment = 1.
-        self.h_sep = 1.
-        self.v_sep = 0.
-        self.rx_area_hcp = 1.
+
+        self.tx_moment = None
+        self.tx_turns = None
+        self.h_sep = None
+        self.v_sep = None
+        self.rx_area_hcp = None
+        self.rx_area_x = None
+        self.rx_area_y = None
+        self.rx_area_z = None
         self.rx_dipole = False
         self.tx_dipole = False
+
         self.frequencies = []
+        self.loop = None
+        self.loop_coords = pd.DataFrame()
         self.data = pd.DataFrame()
 
     def parse(self, filepath):
@@ -37,39 +45,38 @@ class FEMFile:
 
         # The top two lines of headers
         header = split_content[1].split()
-        header.extend(split_content[2].split())
+        header.extend([split_content[2]])
         header_dict = {}
         for match in header:
             value = match.split(':')
             header_dict[value[0]] = value[1]
 
-        # TODO Need more frequencies
+        # Loop name
+        loop = split_content[3][5:-2]  # Remove "LOOP:" and " &"
 
+        # Parse the loop coordinates
+        loop_coords_match = [c for c in split_content if 'LV' in c.upper()]
+        loop_coords = []
+        for match in loop_coords_match:
+            if 'LV' in match:
+                values = [re.search(r'LV\d+\w:(.*)', m).group(1) for m in match.strip().split(' ')]
+                loop_coords.append(values)
+        loop_coords = pd.DataFrame(loop_coords, columns=['Easting', 'Northing', 'Elevation']).astype(float)
+
+        # TODO Need more frequencies
         global frequencies, cols
         frequencies = content.split(r'/FREQ=')[1].split('\n')[0].split(',')
+        assert frequencies, f"No frequencies found."
 
         # Data
-
-        def data_to_readings(row):
-            reading = row.iloc[len(cols):len(ch_times) - len(cols)].to_numpy().astype(float)
-            return reading
-
         data_match = content.split(r'/PROFILEX:')[1].split('\n')[1:]
         # Headers that are always there (?)
         cols = ['Easting', 'Northing', 'Elevation', 'Station', 'Component', 'Dircosz', 'Dircose', 'Dircosn']
-        data = pd.DataFrame([match.split() for match in data_match[:-1]])
+        cols.extend(frequencies)
+        cols.extend(['Distance', 'Calc_this'])
+        data = pd.DataFrame([match.split() for match in data_match[:-1]], columns=cols)
         data.iloc[:, 0:4] = data.iloc[:, 0:4].astype(float)
         data.iloc[:, 5:] = data.iloc[:, 5:].astype(float)
-
-        # Combine the channel column readings into numpy arrays
-        readings = data.apply(data_to_readings, axis=1)
-
-        # Remove the old channel columns
-        data = data.drop(columns=range(len(cols), len(cols) + len(ch_times)))
-        # Update the column names
-        cols.extend(['Distance', 'Calc_this', 'Reading'])
-        data['Reading'] = readings
-        data.columns = cols
 
         # Set the attributes
         self.line = header_dict['LINE']
@@ -77,23 +84,30 @@ class FEMFile:
         self.elevation = header_dict['ELEV']
         self.units = header_dict['ELEV']
         self.current = header_dict['CURRENT']
-        self.tx_turns = header_dict['TXTURNS']
-        self.base_freq = header_dict['BFREQ']
-        self.duty_cycle = header_dict['DUTYCYCLE']
-        self.on_time = header_dict['ONTIME']
-        self.off_time = header_dict['OFFTIME']
-        self.turn_on = header_dict['TURNON']
-        self.turn_off = header_dict['TURNOFF']
-        self.timing_mark = header_dict['TIMINGMARK']
-        self.rx_area_x = header_dict['RXAREAX']
-        self.rx_area_y = header_dict['RXAREAY']
-        self.rx_area_z = header_dict['RXAREAZ']
-        self.rx_dipole = header_dict['RXDIPOLE']
-        self.tx_dipole = header_dict['TXDIPOLE']
+        self.rx_dipole = True if header_dict['RXDIPOLE'] == 'YES' else False
+        self.tx_dipole = True if header_dict['TXDIPOLE'] == 'YES' else False
+
+        if self.rx_dipole:
+            self.rx_area_hcp = header_dict['RXAREAHCP']
+        else:
+            # TODO See if there's RX Area when RX is not dipole
+            self.rx_area_x = header_dict['RXAREAX']
+            self.rx_area_y = header_dict['RXAREAY']
+            self.rx_area_z = header_dict['RXAREAZ']
+
+        if self.tx_dipole:
+            self.tx_moment = header_dict['TXMOMENT']
+        else:
+            self.tx_turns = header_dict['TXTURNS']
+
+        # TODO make sure this is true, that sep and vsep only exist when both rx and tx are dipoles
+        if self.tx_dipole and self.rx_dipole:
+            self.h_sep = header_dict['SEP']
+            self.v_sep = header_dict['VSEP']
+
+        self.frequencies = frequencies
         self.loop = loop
         self.loop_coords = loop_coords
-        self.ch_times = ch_times
-        self.ch_widths = ch_widths
         self.data = pd.DataFrame()
 
         return self
@@ -102,5 +116,6 @@ class FEMFile:
 if __name__ == '__main__':
     fem = FEMFile()
 
-    file = r'C:\Users\Mortulo\PycharmProjects\IRAP_Modelling\sample_files\Maxwell files\Test #2.fem'
+    # file = r'C:\Users\Mortulo\PycharmProjects\IRAP_Modelling\sample_files\Maxwell files\Test #2.fem'
+    file = r'C:\Users\kajth\PycharmProjects\IRAP_Modelling\sample_files\Maxwell files\Test 4 FEM files\Test 4 - h=5m.fem'
     fem_file = fem.parse(file)
